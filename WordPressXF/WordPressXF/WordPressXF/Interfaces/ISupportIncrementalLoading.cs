181b1af9d2c7db17412f451ea26078601f96f89b
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-12 14:55:35 +0100

    - add isincrementalloading property to interface

--- WordPressXF/WordPressXF/WordPressXF/Interfaces/ISupportIncrementalLoading.cs
+++ WordPressXF/WordPressXF/WordPressXF/Interfaces/ISupportIncrementalLoading.cs
@@ -5,6 +5,7 @@ namespace WordPressXF.Interfaces
     public interface ISupportIncrementalLoading
     {
         int PageSize { get; set; }
+        bool IsIncrementalLoading { get; set; }
         bool HasMoreItems { get; set; }
         ICommand LoadMoreItemsCommand { get; }
     }
