52a7b6a704f53270154f69f07073a7ccbc9ce4f3
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-12 13:40:18 +0100

    - add bootstrapper

---
+++ WordPressXF/WordPressXF/WordPressXF/Init/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Practices.ServiceLocation;
+using Microsoft.Practices.Unity;
+using WordPressXF.Services;
+using WordPressXF.ViewModels;
+
+namespace WordPressXF.Init
+{
+    public static class Bootstrapper
+    {
+        public static void RegisterDependencies()
+        {
+            var container = new UnityContainer();
+
+            // service
+            container.RegisterType<WordpressService>(new ContainerControlledLifetimeManager());
+
+            // viewmodel
+            container.RegisterType<NewsViewModel>(new ContainerControlledLifetimeManager());
+
+            var locator = new UnityServiceLocator(container);
+            ServiceLocator.SetLocatorProvider(() => locator);
+        }
+    }
+}
