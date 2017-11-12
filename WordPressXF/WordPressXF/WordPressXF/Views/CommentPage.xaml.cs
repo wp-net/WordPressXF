d7507717004838907ed72f16179d747b5b70329e
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-12 13:38:15 +0100

    - add basic pages

---
+++ WordPressXF/WordPressXF/WordPressXF/Views/CommentPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WordPressXF.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class CommentPage : ContentPage
+	{
+		public CommentPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
