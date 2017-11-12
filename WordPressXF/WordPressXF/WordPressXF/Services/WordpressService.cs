0b76502c08bd293c3e579982b0c5ba985f2c9464
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-12 13:30:55 +0100

    - add wordpress service

---
+++ WordPressXF/WordPressXF/WordPressXF/Services/WordpressService.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using WordPressPCL;
+using WordPressPCL.Models;
+using WordPressPCL.Utility;
+using WordPressXF.Utils;
+
+namespace WordPressXF.Services
+{
+    public class WordpressService
+    {
+        private readonly WordPressClient _client;
+
+        public WordpressService()
+        {
+            _client = new WordPressClient(Statics.WordpressUrl);
+        }
+
+        public async Task<IEnumerable<Post>> GetLatestPostsAsync(int page = 0, int perPage = 20)
+        {
+            page++;
+
+            var posts = await _client.Posts.Query(new PostsQueryBuilder
+            {
+                Page = page,
+                PerPage = perPage,
+                Embed = true
+            });
+
+            return posts;
+        }
+        
+        public async Task<List<CommentThreaded>> GetCommentsForPostAsync(int postid)
+        {        
+            var comments = await _client.Comments.Query(new CommentsQueryBuilder
+            {
+                Posts = new[] { postid },
+                Page = 1,
+                PerPage = 100
+            });
+            
+            return ThreadedCommentsHelper.GetThreadedComments(comments);
+        }
+    }
+}
---
+++ WordPressXF/WordPressXF/WordPressXF/Utils/Statics.cs
@@ -0,0 +1,7 @@
+﻿namespace WordPressXF.Utils
+{
+    public static class Statics
+    {
+        public static string WordpressUrl = "http://www.example.com/wp-json/";
+    }
+}
