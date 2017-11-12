203e69ef06c301142cd364e9a0a84116311a4faa
Sebastian Jensen <apps@tsjdev-apps.de>
2017-11-12 14:31:14 +0100

    - add converter to change visibility of elements

---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/NullOrEmptyListToIsVisibleConverter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    public class NullOrEmptyListToIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch (value)
+            {
+                case null:
+                    return parameter == null;
+                case IList list:
+                    return list.Count == 0;
+                default:
+                    return false;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
