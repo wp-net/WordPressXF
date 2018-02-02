eb16a779cac221dac3e7a0b85929c97fb0a5b442
Sebastian Jensen <apps@tsjdev-apps.de>
2018-02-02 16:19:08 +0100

    - add settings page

---
+++ WordPressXF/WordPressXF/WordPressXF/Views/SettingsPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using CommonServiceLocator;
+using WordPressXF.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace WordPressXF.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SettingsPage : TabbedPage
+    {
+        public SettingsPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ServiceLocator.Current.GetInstance<SettingsViewModel>();
+        }
+    }
+}
