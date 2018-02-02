3c77f27630520795b4ad1e1c6e57f83cc63074f2
Sebastian Jensen <apps@tsjdev-apps.de>
2018-02-02 16:18:43 +0100

    - add image source converter

---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/ImageSourceConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    public class ImageSourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var imagePath = value as string;
+            if (string.IsNullOrEmpty(imagePath))
+                return null;
+
+            var resource = ImageSource.FromResource(imagePath);
+            return resource;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
