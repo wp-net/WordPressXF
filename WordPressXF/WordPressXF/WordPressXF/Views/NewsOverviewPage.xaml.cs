4016c996095cfa136cd7a63ab57a70324b2286cd
Sebastian Jensen <apps@tsjdev-apps.de>
2018-02-02 16:19:43 +0100

    - update news overview

--- WordPressXF/WordPressXF/WordPressXF/Views/NewsOverviewPage.xaml.cs
+++ WordPressXF/WordPressXF/WordPressXF/Views/NewsOverviewPage.xaml.cs
@@ -1,14 +1,18 @@
-﻿using Xamarin.Forms;
+﻿using CommonServiceLocator;
+using WordPressXF.ViewModels;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WordPressXF.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class NewsOverviewPage : ContentPage
-	{
-		public NewsOverviewPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NewsOverviewPage : ContentPage
+    {
+        public NewsOverviewPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ServiceLocator.Current.GetInstance<NewsViewModel>();
+        }
+    }
 }
