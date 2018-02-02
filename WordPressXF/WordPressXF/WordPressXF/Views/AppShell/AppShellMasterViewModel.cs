119f88d781a475b81f77e32a1fc15ce491827a07
Sebastian Jensen <apps@tsjdev-apps.de>
2018-02-02 16:18:59 +0100

    - add appshell

---
+++ WordPressXF/WordPressXF/WordPressXF/Views/AppShell/AppShellMasterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.ObjectModel;
+
+namespace WordPressXF.Views.AppShell
+{
+    public class AppShellMasterViewModel
+    {
+        public ObservableCollection<AppShellMenuItem> PrimaryMenuItems { get; set; }
+        public ObservableCollection<AppShellMenuItem> SecondaryMenuItems { get; set; }
+
+        public AppShellMasterViewModel()
+        {
+            PrimaryMenuItems = new ObservableCollection<AppShellMenuItem>(new[]
+            {
+                new AppShellMenuItem { Id = 0, Title = "News", TargetType = typeof(NewsOverviewPage) },
+            });
+
+            SecondaryMenuItems = new ObservableCollection<AppShellMenuItem>(new[]
+            {
+                new AppShellMenuItem { Id = 0, Title = "Settings", TargetType = typeof(SettingsPage) },
+            });
+        }
+    }
+}
