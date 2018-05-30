2981d9e63f7db493dacf5cae5105cc09d3287ab4
Sebastian Jensen <apps@tsjdev-apps.de>
2018-05-30 21:47:46 +0200

    - add converter for checking if a user is logged in

---
+++ WordPressXF/WordPressXF/WordPressXF/Converters/CurrentUserToIsVisibleConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace WordPressXF.Converters
+{
+    public class CurrentUserToIsVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+            {
+                return parameter == null;
+            }
+
+            return parameter != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
