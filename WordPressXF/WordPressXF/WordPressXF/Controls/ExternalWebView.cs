9b3321f712e03ccc11fbad56f2a551b9cfab9b9b
Sebastian Jensen <apps@tsjdev-apps.de>
2018-06-02 22:30:45 +0200

    - add externalwebview as control

---
+++ WordPressXF/WordPressXF/WordPressXF/Controls/ExternalWebView.cs
@@ -0,0 +1,32 @@
+﻿using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace WordPressXF.Controls
+{
+    public class ExternalWebView : WebView
+    {
+        public ExternalWebView()
+        {
+            Navigating += ExternalWebViewOnNavigating;
+        }
+
+        private async void ExternalWebViewOnNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            if (!e.Url.StartsWith("http"))
+                return;
+
+            if (!e.Url.StartsWith("https"))
+                return;
+
+            if (e.Url.StartsWith("https://www.youtube.com/"))
+                return;
+
+            if (e.Url.StartsWith("https://syndication.twitter.com/") || e.Url.StartsWith("https://platform.twitter.com/"))
+                return;
+
+            e.Cancel = true;
+
+            await Browser.OpenAsync(e.Url, BrowserLaunchType.External);
+        }
+    }
+}
