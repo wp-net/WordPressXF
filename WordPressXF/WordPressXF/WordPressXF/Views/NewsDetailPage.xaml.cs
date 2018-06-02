91c767c5ae1afe3a0bb3687afdd648f097952d91
Sebastian Jensen <apps@tsjdev-apps.de>
2018-06-02 22:31:50 +0200

    - use externalwebview

--- WordPressXF/WordPressXF/WordPressXF/Views/NewsDetailPage.xaml.cs
+++ WordPressXF/WordPressXF/WordPressXF/Views/NewsDetailPage.xaml.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace WordPressXF.Views
@@ -13,18 +12,6 @@ namespace WordPressXF.Views
 
             if (Device.RuntimePlatform == Device.iOS)
                 Icon = new FileImageSource { File = "post.png" };
-        }
-
-        private void WebViewOnNavigating(object sender, WebNavigatingEventArgs e)
-        {
-            if (!e.Url.StartsWith("http"))
-                return;
-
-            if (e.Url.StartsWith("https://syndication.twitter.com/") || e.Url.StartsWith("https://platform.twitter.com/"))
-                return;
-
-            e.Cancel = true;
-            Device.OpenUri(new Uri(e.Url));
         }
     }
 }
